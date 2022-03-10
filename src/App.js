e598555cbcc77576cb56a2e94c51d6d0ff88b7f1
Balogun Ademola <Balogun80@gmail.com>
2022-03-10 01:49:43 -0800

    new changes

--- src/App.js
+++ src/App.js
@@ -15,13 +15,14 @@ function App() {
   
   return (
     <>
+     {answer ? "The digits are increasing" : "The digits are decreasing" }
 
       <h3 id = "header">Counter</h3>
       <button id = "counter-increment" onClick ={increment} >+</button>
       <button id = "counter-decrement"  onClick ={decrement} >-</button>
       <h3 id = "answers">{answer}</h3>
       <h3 id = "answersTwo">{answerTwo}</h3>
-     {answer ?  <strong>The digits are increasing</strong> : <strong>The digits are decreasing</strong> }
+    
 
     </>
   );
